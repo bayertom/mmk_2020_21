fdeb8d3e5dace38d9f5da1fe4985b04d3b63d0fc
bayertom <bayertom@natur.cuni.cz>
2021-05-12 00:10:38 +0200

    CV13

--- mmk_optimal_proj2.m
+++ mmk_optimal_proj2.m
@@ -7,10 +7,10 @@ u_min = -90;
 u_max = 90;
 v_min = -180;
 v_max = 180;
-u_min = 50;
-u_max = 50;
-v_min = 15;
-v_max = 15;
+% u_min = 50;
+% u_max = 50;
+% v_min = 15;
+% v_max = 15;
 du = 10;
 dv = 10;
 R = 6380;
@@ -18,7 +18,7 @@ R = 6380;
 [u,v] = meshgrid(u_min:du:u_max, v_min:dv:v_max);
 
 %Sinusoidalni zobrazeni
-[x,y] = sinu(R,u,v,0);
+[X,Y] = sinu(R,u,v,0);
 
 %Zkresleni
 mp_2=((v/180*pi).*sin(u/180*pi)).^2+1;
@@ -37,7 +37,7 @@ a_2 = mp_2 .* (cos(A1)).^2 + mr_2 .* (sin(A1)).^2 +  p.*sin(A1).*cos(A1);
 b_2 = mp_2 .* (cos(A2)).^2 + mr_2 .* (sin(A2)).^2 +  p.*sin(A2).*cos(A2);
 a = sqrt(a_2);
 b = sqrt(b_2);
-m2 = max(a_2,b_2);
+m2_max = max(a_2,b_2);
 
 %Airy kriterium
 h_2A = ((a-1).^2 + (b-1).^2)/2;
@@ -60,6 +60,4 @@ H1_A = sqrt(H1_2_A);
 H1_C = sqrt(H1_2_C);
 H2_A = sqrt(H2_2_A);
 H2_C = sqrt(H2_2_C);
-
-
 
